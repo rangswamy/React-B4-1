4819859044bc2318dd5e90bfa2564b6c52627046
ashokreactdeveloper <ashoksivagnanam83@gmail.com>
2024-05-10 07:07:40 +0530

    My first git commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "./redux/features/products/productsSlice";
+import Products from "./components/Products";
+import Loading from "./components/Loading";
 
 function App() {
+
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.productsReducer.loading);
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? <Loading /> : <Products />}
     </div>
   );
 }
