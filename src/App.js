e24f105aaa0def683a04bac728f4cd36bf299cb4
pooja <kpoojakulkarni@gmail.com>
2024-05-16 20:39:01 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import Contact from "./components/Contact";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 
 function App() {
