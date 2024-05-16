cbce79a57ac1a87e177c5c8c89c8f2938bfcd2a7
venkatesh <venkateshkpatil333@gmail.com>
2024-05-16 20:01:25 +0530

    change the code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "./redux/features/products/productsSlice";
-import Products from "./components/Products";
-import Loading from "./components/Loading";
+// import React, { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { getProducts } from "./redux/features/products/productsSlice";
+// import Products from "./components/Products";
+// import Loading from "./components/Loading";
+
+import About from "./components/About";
+import Team from "./components/Team";
 
 function App() {
 
-  const dispatch = useDispatch();
-  const loading = useSelector(state => state.productsReducer.loading);
+  // const dispatch = useDispatch();
+  // const loading = useSelector(state => state.productsReducer.loading);
 
-  useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch])
+  // useEffect(() => {
+  //   dispatch(getProducts());
+  // }, [dispatch])
 
   return (
     <div>
-      {loading ? <Loading /> : <Products />}
+      {/* {loading ? <Loading /> : <Products />} */}
+      <About/>
+      <Team/>
     </div>
   );
 }
---
+++ src/components/Team.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import {FaInstagram,FaLinkedinIn} from 'react-icons/fa';
+import './Team.css';
+
+
+const Team = () => {
+    return (
+        <div>
+            <div className="team-info">
+                <p>Team Member</p>
+                <h2>Meet Our Expert Trainers</h2>
+            </div>
+            <div className="team-one">
+                <div className="profile-one">
+                    <img className='mem-image' src='https://www.amigosbusinesscorp.com/img/bharat.jpg' alt='bharat '></img>
+                    <h3>Bharat kumar T</h3>
+                    <p>Administrator</p>
+                    <div className='team-social'>
+                        <FaLinkedinIn className='a' />
+                        <FaInstagram className='a' />
+                    </div>
+                </div>
+
+
+                <div className="profile-one">
+                    <img className='mem-image' src='https://www.amigosbusinesscorp.com/img/swamy.jpg' alt=' '></img>
+                    <h3>Swamy R</h3>
+                    <p>Co-ordinator</p>
+                    <div className='team-social'>
+                        <FaLinkedinIn className='a' />
+                        <FaInstagram className='a' />
+                    </div>
+                </div>
+
+                <div className="profile-one">
+                    <img className='mem-image' src='https://www.amigosbusinesscorp.com/img/praveen.jpg' alt=' '></img>
+                    <h3>Praveen K</h3>
+                    <p>Trainer</p>
+                    <div className='team-social'>
+                        <FaLinkedinIn className='a' />
+                        <FaInstagram className='a' />
+                    </div>
+                </div>
+
+                <div className="profile-one">
+                    <img className='mem-image' src='https://www.amigosbusinesscorp.com/img/adi.jpg' alt=' '></img>
+                    <h3>Adikarthik Gupta</h3>
+                    <p>Trainer</p>
+                    <div className='team-social'>
+                        <FaLinkedinIn className='a' />
+                        <FaInstagram className='a' />
+                    </div>
+                </div>
+            </div>
+
+            <div className="team-two">
+
+                <div className="profile-one">
+                    <img className='mem-image' src='https://www.amigosbusinesscorp.com/img/harish.jpg' alt=' '></img>
+                    <h3>Harish</h3>
+                    <p>Trainer</p>
+                    <div className='team-social'>
+                        <FaLinkedinIn className='a' />
+                        <FaInstagram className='a' />
+                    </div>
+                </div>
+
+                <div className="profile-one">
+                    <img className='mem-image' src='https://www.amigosbusinesscorp.com/img/shashi.jpg' alt=' '></img>
+                    <h3>Shashikala</h3>
+                    <p>Trainer</p>
+                    <div className='team-social'>
+                        <FaLinkedinIn className='a' />
+                        <FaInstagram className='a' />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Team;
