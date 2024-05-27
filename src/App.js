4e6d7115cc03c5be3310995dc7c826cb2ea377a1
Nikhil <nikhilk84310@gmail.com>
2024-05-27 20:51:50 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import FAQ from './components/Accordion/FAQ';
-import Footer from './components/Footer/Footer';
+import FAQ from "./components/Accordion/FAQ";
+import Footer from "./components/Footer/Footer";
 import Team from "./components/Team/Team";
 import LoginSignup from "./components/LoginSignup/LoginSignup";
+import Services from "./components/Services/Services"
+import FrequentlyAskedQuestions from "./components/FAQ/FrequentlyAskedQuestions"
 import './App.css';
 
 
 function App() {
   return (
     <div>
-       <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path='/' element={<><LoginSignup /></>} />
-          <Route path='/team' element={<><FAQ /><Team /><Footer /></>} />
+          <Route path='/team' element={<><Services /><FrequentlyAskedQuestions /><FAQ /><Team /><Footer /></>} />
         </Routes>
       </BrowserRouter>
     </div>
